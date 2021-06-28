973bcc8e08c04ac8ed1543bf9a249c46f20fd837
EdierDavidME <edierdavid000@gmail.com>
2021-06-28 16:34:31 -0500

    Version 1.2.1

---
+++ src/components/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { User } from '../model/Model';
+import {AuthService } from '../services/AuthService';
+
+interface AppState {
+user: User | undefined
+}
+export class App extends React.Component<{}, AppState>{
+
+    private authService: AuthService = new AuthService();
+
+    render(){
+        return(
+            <div>La clase de la app funciona</div>
+        );
+    }
+}
